8440079ad2029cf7af50bf6076b62d4c2ac7d0ec
ross.oreto <ross.oreto@gmail.com>
2018-05-30 11:32:52 -0500

    move auth file check to after directory creation.

--- create-server.groovy
+++ create-server.groovy
@@ -15,21 +15,18 @@ String mainVert = this.args.length > 1 ? this.args[1] : 'org.oreto.vertx.server.
 
 String first = Files.exists(Paths.get('.', 'build.gradle')) ? '..' : '.'
 def projectDir = new File(Paths.get(first, name).toString())
-if (first =='..') {
-    Path authFile = Paths.get('.', 'auth')
-    if (Files.exists(authFile)) {
-        Files.copy(authFile, Paths.get(first, name, 'auth'))
-    }
-}
+
 GroovyShell shell = new GroovyShell()
 String updateFileName = 'update.groovy'
 Path updateFilePath = Paths.get(first, name, updateFileName)
 if (projectDir.exists()) {
+    checkForAuthFile(first, name)
     println("$name already exists")
     shell.run(new File(updateFilePath.toString()), ['/', projectDir.path])
     createMainVerticle(first, name, mainVert)
 } else {
     projectDir.mkdir()
+    checkForAuthFile(first, name)
     RESTClient client = new RESTClient('https://api.github.com')
     client.parser.'application/vnd.github.v3.raw' = client.parser.'application/octet-stream'
     HttpResponseDecorator response = client.get(['path': '/repos/ross-oreto/vertx-server/contents/' + updateFileName, 'headers': [
@@ -42,6 +39,15 @@ if (projectDir.exists()) {
     Files.write(updateFilePath, bytes)
     shell.run(new File(updateFilePath.toString()), ['/', projectDir.path])
     createMainVerticle(first, name, mainVert)
+}
+
+static void checkForAuthFile(String first, String project) {
+    if (first =='..') {
+        Path authFile = Paths.get('.', 'auth')
+        if (Files.exists(authFile)) {
+            Files.copy(authFile, Paths.get(first, project, 'auth'))
+        }
+    }
 }
 
 static void createMainVerticle(String first, String project, String vert) {
